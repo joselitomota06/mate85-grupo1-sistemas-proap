3e4f5ad8769d53d2655702e85f18cc87765cc45a
João Pedro Brito Silva <joao.brito@solutis.com.br>
2022-09-17 12:01:58 -0300

    feat: implementação do helper para validação de CPFs

--- proap-web/src/containers/register/RegisterFormSchema.ts
+++ proap-web/src/containers/register/RegisterFormSchema.ts
@@ -1,4 +1,5 @@
 import * as Yup from 'yup'
+import { validateCPF } from '../../helpers/validation'
 
 export const registerFormSchema = Yup.object({
   email: Yup.string()
@@ -13,28 +14,8 @@ export const registerFormSchema = Yup.object({
     }),
   cpf: Yup.string()
     .required('Campo obrigatório')
-    .test('validation-cpf', 'CPF inválido', function (value) {
-      var Soma
-      var Resto
-      var i
-      Soma = 0
-      if (!value || value == '00000000000') return false
-
-      for (i = 1; i <= 9; i++)
-        Soma = Soma + parseInt(value.substring(i - 1, i)) * (11 - i)
-      Resto = (Soma * 10) % 11
-
-      if (Resto == 10 || Resto == 11) Resto = 0
-      if (Resto != parseInt(value.substring(9, 10))) return false
-
-      Soma = 0
-      for (i = 1; i <= 10; i++)
-        Soma = Soma + parseInt(value.substring(i - 1, i)) * (12 - i)
-      Resto = (Soma * 10) % 11
-
-      if (Resto == 10 || Resto == 11) Resto = 0
-      if (Resto != parseInt(value.substring(10, 11))) return false
-      return true
+    .test('validation-cpf', 'CPF inválido', function (cpf) {
+      return validateCPF(cpf)
     }),
   name: Yup.string().required('Campo obrigatório'),
   registration: Yup.string().required('Campo obrigatório'),
