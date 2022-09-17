d32d19ef3673f85e720877784912a3da25c87986
Lucas Carvalho <lucasyancarvalho@gmail.com>
2022-09-17 20:50:56 -0300

    fix: remoção imports sem uso

--- proap-api/src/main/java/br/ufba/proap/authentication/domain/User.java
+++ proap-api/src/main/java/br/ufba/proap/authentication/domain/User.java
@@ -6,8 +6,6 @@ import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
