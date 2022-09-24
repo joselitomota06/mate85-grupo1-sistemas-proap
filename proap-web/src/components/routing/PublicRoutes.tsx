398e381b8926d3990f9e43a8d669e8d19e6b7803
Itamar Joire <30444253+ItamarJoire@users.noreply.github.com>
2022-09-24 10:16:58 -0300

    Página de recuperação de senha (#10)
    * Página de recuperação de senha
    
    * Correções feitas
    
    * fix: inclusão do handler vazio no submit do formulário de recuperação

--- proap-web/src/components/routing/PublicRoutes.tsx
+++ proap-web/src/components/routing/PublicRoutes.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { RegisterPage, LoginPage } from '../../pages'
+import { RegisterPage, RecoverPasswordPage, LoginPage } from '../../pages'
 
 export default function PublicRoutes() {
   return (
     <Routes>
       <Route path='register' element={<RegisterPage />} />
+      <Route path='recover-password' element={<RecoverPasswordPage />} />
       <Route path='*' element={<LoginPage />} />
     </Routes>
   )
