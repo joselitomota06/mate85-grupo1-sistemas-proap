843950acbb9609edaeb9022cf249fcefbc45a518
AirtonSerra <airton_serra_sena@hotmail.com>
2022-10-09 13:47:17 -0300

    feat: adicionado validação de campos na tela de recuperação de senha

--- proap-web/src/containers/recover-password/RecoverPasswordSchema.ts
+++ proap-web/src/containers/recover-password/RecoverPasswordSchema.ts
@@ -1,13 +1,31 @@
 import * as Yup from 'yup'
+import { validateCPF } from '../../helpers'
 
 export interface RecoverPasswordFormValues {
-  email: string
+  email: string,
+  password: string,
+  confirmPassword: string
+  cpf: string
 }
 
 export const INITIAL_FORM_VALUES: RecoverPasswordFormValues = {
   email: '',
+  password: '',
+  confirmPassword: '',
+  cpf: ''
 }
 
 export const recoverPasswordFormSchema = Yup.object({
   email: Yup.string().required('Campo obrigatório'),
+  cpf: Yup.string()
+    .required('Campo obrigatório')
+    .test('validation-cpf', 'CPF inválido', function (cpf) {
+      return validateCPF(cpf)
+    }),
+  password: Yup.string().required('Campo obrigatório'),
+  confirmPassword: Yup.string()
+  .required('Campo obrigatório')
+  .test('same-password', 'As senhas não coincidem', function (value) {
+    return this.parent.password == value
+  }),
 })
