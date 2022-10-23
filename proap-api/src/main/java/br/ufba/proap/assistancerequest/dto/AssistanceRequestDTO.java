674bad135742969c295c98620556a2c49407623b
Lucas Carvalho <lucasyancarvalho@gmail.com>
2022-10-22 21:31:31 -0300

    feature: novo objeto Review para armazenar a aprovação/reprovação do Request

--- proap-api/src/main/java/br/ufba/proap/assistancerequest/dto/AssistanceRequestDTO.java
+++ proap-api/src/main/java/br/ufba/proap/assistancerequest/dto/AssistanceRequestDTO.java
@@ -4,13 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.springframework.integration.annotation.Default;
 
@@ -73,6 +67,10 @@ public class AssistanceRequestDTO {
 	private String doi;
 	
 	private String autores;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "review_id", referencedColumnName = "id")
+	private Review review;
 	
 	@Override
 	public int hashCode() {
---
+++ proap-api/src/main/java/br/ufba/proap/assistancerequest/dto/Review.java
@@ -0,0 +1,73 @@
+package br.ufba.proap.assistancerequest.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "proap_review", schema = "proap")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String numeroAta;
+
+    private Date dataAprovacao;
+
+    @Column(nullable = false)
+    private int numeroDiariasAprovadas;
+
+    private String observacao;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "review")
+    private AssistanceRequestDTO assistanceRequestDTO;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AssistanceRequestDTO getAssistanceRequestDTO() {
+        return assistanceRequestDTO;
+    }
+
+    public void setAssistanceRequestDTO(AssistanceRequestDTO assistanceRequestDTO) {
+        this.assistanceRequestDTO = assistanceRequestDTO;
+    }
+
+    public String getNumeroAta() {
+        return numeroAta;
+    }
+
+    public void setNumeroAta(String numeroAta) {
+        this.numeroAta = numeroAta;
+    }
+
+    public Date getDataAprovacao() {
+        return dataAprovacao;
+    }
+
+    public int getNumeroDiariasAprovadas() {
+        return numeroDiariasAprovadas;
+    }
+
+    public void setNumeroDiariasAprovadas(int numeroDiariasAprovadas) {
+        this.numeroDiariasAprovadas = numeroDiariasAprovadas;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}
