8e4e7477f513dcb18c5ca2e3ba772c50b700cc25
Joselito Jr <joselito.mota@ufba.br>
2022-11-02 20:20:25 -0300

    Adding review and user to assistanceRequestSlice interface

--- proap-web/src/store/slices/assistance-request-slice/assistanceRequestSlice.ts
+++ proap-web/src/store/slices/assistance-request-slice/assistanceRequestSlice.ts
@@ -4,6 +4,26 @@ export interface AssistanceRequest {
   id: number
   doi: string
   nomeCompleto: string
+  valorSolicitado: number
+  valorInscricao: number
+  review : {
+    createdAt: string,
+    dataAprovacao: string,
+    id: number,
+    numeroAta: string,
+    numeroDiariasAprovadas: number,
+    observacao: string,
+    situacao: number,
+    updatedAt: string
+  }
+  user : {
+    id: number
+    alternativePhone: string
+    cpf: string
+    email: string
+    name: string
+    password: string
+  }
 }
 
 interface AssistanceRequestSliceState {
