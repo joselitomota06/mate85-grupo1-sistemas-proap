e74341ecf969d8c74e08555d6a53761f6f0bf6cf
Joselito Jr <joselito.mota@ufba.br>
2022-11-07 16:27:18 -0300

    Fixing paper name variable

--- proap-web/src/store/slices/assistance-request-slice/assistanceRequestSlice.ts
+++ proap-web/src/store/slices/assistance-request-slice/assistanceRequestSlice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 export interface AssistanceRequest {
   id: number
   doi: string
-  nomeCompleto: string
+  nomeSolicitante: string
   valorSolicitado: number
   valorInscricao: number,
   createdAt: string,
