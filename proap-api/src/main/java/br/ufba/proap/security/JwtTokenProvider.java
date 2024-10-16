240513e37a6f7d56be4f0414137ee41b3744bdc2
wallacemendes <wallace.bmj@gmail.com>
2024-10-15 21:29:57 -0300

    refactor JWT impl

--- proap-api/src/main/java/br/ufba/proap/security/JwtTokenProvider.java
+++ proap-api/src/main/java/br/ufba/proap/security/JwtTokenProvider.java
@@ -12,12 +12,10 @@ import org.springframework.stereotype.Component;
 
 import br.ufba.proap.authentication.domain.User;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.Password;
 
 @Component
 public class JwtTokenProvider {
@@ -29,36 +27,41 @@ public class JwtTokenProvider {
 	private int jwtExpirationInMs;
 
 	/*
-	@Autowired
-	private PerfilService perfilService; */
+	 * @Autowired
+	 * private PerfilService perfilService;
+	 */
 
 	public String generateToken(Authentication authentication) {
 		User userPrincipal = (User) authentication.getPrincipal();
 		OffsetDateTime expiryDate = OffsetDateTime.now().plus(jwtExpirationInMs, ChronoUnit.MILLIS);
 		Map<String, Object> claims = new HashMap<>();
+		Password secretKey = Keys.password(jwtSecret.toCharArray());
 
 		claims.put("id", userPrincipal.getId());
 		claims.put("name", userPrincipal.getName());
 		claims.put("email", userPrincipal.getEmail());
 		claims.put("isAdmin", userPrincipal.getPerfil() != null ? userPrincipal.getPerfil().isAdmin() : false);
 
-		return Jwts.builder().setSubject(userPrincipal.getName()).setClaims(claims)
-				.setIssuedAt(Date.from(OffsetDateTime.now().toInstant()))
-				.setExpiration(Date.from(expiryDate.toInstant())).signWith(SignatureAlgorithm.HS256, jwtSecret)
-				.compact();
+		return Jwts.builder()
+				.subject(userPrincipal.getName())
+				.claims(claims)
+				.issuedAt(Date.from(OffsetDateTime.now().toInstant()))
+				.expiration(Date.from(expiryDate.toInstant()))
+				.signWith(secretKey).compact();
 	}
 
 	String getEmailFromJwt(String token) {
-		Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+		Password secretKey = Keys.password(jwtSecret.toCharArray());
+		Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
 		return String.valueOf(claims.get("email"));
 	}
 
 	boolean validateToken(String authToken) {
+		Password secretKey = Keys.password(jwtSecret.toCharArray());
 		try {
-			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(authToken);
 			return true;
-		} catch (ExpiredJwtException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException
-				| SignatureException e) {
+		} catch (JwtException | IllegalArgumentException e) {
 			e.getMessage();
 		}
 		return false;
