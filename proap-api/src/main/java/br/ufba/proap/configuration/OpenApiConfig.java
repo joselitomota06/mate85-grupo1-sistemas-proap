50db28d01521e171f347f4244bccf542ef64a7ff
wallacemendes <wallace.bmj@gmail.com>
2024-10-17 22:52:06 -0300

    refact: Adjusts to Swagger constants

--- proap-api/src/main/java/br/ufba/proap/configuration/OpenApiConfig.java
+++ proap-api/src/main/java/br/ufba/proap/configuration/OpenApiConfig.java
@@ -6,9 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
+import br.ufba.proap.configuration.constant.SwaggerConstant;
 
-@OpenAPIDefinition(info = @Info(title = "Proap API", description = "API description here", version = "v1"), security = @SecurityRequirement(name = "bearerAuth"))
-@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+@OpenAPIDefinition(info = @Info(title = SwaggerConstant.API_TITLE, description = SwaggerConstant.API_DESCRIPTION, version = SwaggerConstant.API_VERSION), security = @SecurityRequirement(name = SwaggerConstant.AUTHORIZATION_HEADER))
+@SecurityScheme(name = SwaggerConstant.AUTHORIZATION_HEADER, type = SecuritySchemeType.HTTP, scheme = SwaggerConstant.AUTHORIZATION_SCHEME, bearerFormat = SwaggerConstant.AUTHORIZATION_FORMAT, description = SwaggerConstant.AUTHORIZATION_DESCRIPTION)
 @Configuration
 public class OpenApiConfig {
         // No explicit bean creation needed for OpenAPI configuration with
--- proap-api/src/main/java/br/ufba/proap/configuration/constant/SwaggerConstant.java
+++ proap-api/src/main/java/br/ufba/proap/configuration/constant/SwaggerConstant.java
@@ -1,12 +1,13 @@
 package br.ufba.proap.configuration.constant;
 
 public class SwaggerConstant {
-    public static final String SECURITY_REFERENCE = "Token Access";
-    public static final String AUTHORIZATION_DESCRIPTION = "Full API Permission";
-    public static final String AUTHORIZATION_SCOPE = "Unlimited";
-    public static final String AUTHORIZATION = "x-access-token";
+
+    public static final String AUTHORIZATION_DESCRIPTION = "Please fill with bearer token below and get authenticated before use other endpoints.";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_SCHEME = "bearer";
+    public static final String AUTHORIZATION_FORMAT = "JWT";
 
     public static final String API_TITLE = "PROAP API";
-    public static final String API_DESCRIPTION = "Please login below and get the token before use other endpoints.";
-    public static final String API_TAG = "PROAP Service";
+    public static final String API_DESCRIPTION = "Proap API for managing requests and users";
+    public static final String API_VERSION = "v1";
 }
