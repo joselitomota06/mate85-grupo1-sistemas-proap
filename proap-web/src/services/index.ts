a5c88e78d60f5b5024feacd638330792c384b2d7
wallacemendes <wallace.bmj@gmail.com>
2025-01-28 21:23:13 -0300

    fix logout session when jwt expires

--- proap-web/src/services/index.ts
+++ proap-web/src/services/index.ts
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import { BASE_API_URL } from '../helpers/api';
 import { LocalStorageToken } from '../helpers/auth';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 
 const api = axios.create({
   baseURL: BASE_API_URL,
 });
 
+const isTokenExpired = (token: string): boolean => {
+  const decoded = jwtDecode<JwtPayload>(token);
+  return decoded.exp ? decoded.exp * 1000 < Date.now() : true;
+};
+
 api.interceptors.request.use(
   (config) => {
     const token = LocalStorageToken.get();
-    if (token)
+    if (token) {
+      if (isTokenExpired(token)) {
+        LocalStorageToken.clear();
+      }
       return {
         ...config,
         headers: {
@@ -17,7 +26,7 @@ api.interceptors.request.use(
           Authorization: `Bearer ${token}`,
         },
       } as any;
-
+    }
     return config;
   },
   (error) => {
