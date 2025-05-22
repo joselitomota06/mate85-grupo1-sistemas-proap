07f48762d048a38bb3c91b8111c94ecf1575bfbf
wallacemendes <wallace.bmj@gmail.com>
2025-05-21 22:46:22 -0300

    chore: configure API_BASE_URL on frontend to respond correclty when running as static files on nginx

--- proap-web/src/helpers/api.ts
+++ proap-web/src/helpers/api.ts
@@ -1,3 +1,9 @@
-export const BASE_API_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:9000/api';
+const getBaseApiURL = () => {
+  if (typeof window !== 'undefined') {
+    return `${window.location.origin}/api`;
+  }
+  return 'http://localhost:3000/api';
+};
+
+export const BASE_API_URL = getBaseApiURL();
 export const BASE_PDF_URL = BASE_API_URL + '/files/view-pdf/';
