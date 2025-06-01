9eab6081ccc099e9792c66dbaa584063b3944807
wallacemendes <wallace.bmj@gmail.com>
2025-05-31 23:46:26 -0300

    refactor: adds alias to Email Service

--- proap-api/src/main/java/br/ufba/proap/mailsender/EmailService.java
+++ proap-api/src/main/java/br/ufba/proap/mailsender/EmailService.java
@@ -3,6 +3,7 @@ package br.ufba.proap.mailsender;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -17,6 +18,11 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class EmailService {
+
+    @Value("${app.mail.from}")
+    private String from;
+    @Value("${app.mail.from-alias}")
+    private String alias;
 
     @Autowired
     private JavaMailSender mailSender;
@@ -64,6 +70,7 @@ public class EmailService {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+            helper.setFrom(from, alias);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(htmlContent, true);
